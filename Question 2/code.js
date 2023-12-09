2d32cc1a9e9a6d0aeaa697a13e5b898cc0d4bd74
Ritesh <100128015+Riteshk229@users.noreply.github.com>
2023-12-09 13:35:21 +0530

    Update code.js

--- Question 2/code.js
+++ Question 2/code.js
@@ -1,19 +1,20 @@
-// Given an integer N. Check whether N is prime or not. (Prime or Not Prime)
+// Given an integer N. Check whether N is prime or not. (Prime or NotPrime)
 // Sample input: N = 10
 // Sample output: NotPrime
+
 let number = prompt("Enter the number to check prime : ");
 
 function checkPrime(num){
     if(num <= 1){
-        return console.log(`${num} is not a prime`);
+        return console.log("Prime");
     }
     
     for(let i = 2; i < Math.sqrt(num); i++){
         if(num % i == 0){
-            return console.log(`${num} is not a prime` );
+            return console.log("NotPrime");
         }
     }
-    return console.log(`${num} is a prime`);
+    return console.log("Prime");
 }
 
 checkPrime(parseInt(number));
